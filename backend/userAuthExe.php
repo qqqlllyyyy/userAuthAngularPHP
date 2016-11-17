76ae0342d2180b1bad5ed4643ca838e54ab6e748
Liyu Qin <Michael@Liyus-iMac.local>
2016-11-16 19:48:58 -0800

    Some updates

--- backend/userAuthExe.php
+++ backend/userAuthExe.php
@@ -4,14 +4,20 @@ include_once('connection.php');
 //--------------------------------------------------------------------------------------
 // 1. Sign Up
 //--------------------------------------------------------------------------------------
+//  1) Get post data
+//  2) Select from database
+//  3) Insert into database
+//  4) Return
+//--------------------------------------------------------------------------------------
 
 if (isset($_GET['action']) && $_GET['action'] == 'signup') {
-  // Get Data
+
+  // Get post data
   $data = json_decode(file_get_contents("php://input"));
   $username = $data -> username;
   $password = $data -> password;
 
-  // Select From Database
+  // Select from database
   $sql = "SELECT * FROM `{$CONFIG['TBL_USER']}`
         WHERE `Username`='{$username}'";
   $userInfo = $DB -> get_all($sql);
@@ -20,7 +26,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'signup') {
     exit();
   }
 
-  // Insert Into Database
+  // Insert into database
   $token = $username . " | " . uniqid() . uniqid() . uniqid();
   $info = array(
     'Username'   => $username,
@@ -40,15 +46,21 @@ if (isset($_GET['action']) && $_GET['action'] == 'signup') {
 //--------------------------------------------------------------------------------------
 // 2. Login
 //--------------------------------------------------------------------------------------
+//  1) Get post data
+//  2) Select from database
+//  3) If loggedin, give him a token
+//  4) Update database with the token
+//  5) Return
+//--------------------------------------------------------------------------------------
 
 if (isset($_GET['action']) && $_GET['action'] == 'login') {
 
-  // Get Post Data
+  // Get post data
   $data = json_decode(file_get_contents("php://input"));
   $username = $data -> username;
   $password = $data -> password;
 
-  // Select From Database
+  // Select from database
   $sql = "SELECT * FROM `{$CONFIG['TBL_USER']}`
         WHERE `Username`='{$username}'
         AND `Password`='" . sha1($password) . "'";
@@ -78,15 +90,19 @@ if (isset($_GET['action']) && $_GET['action'] == 'login') {
 //--------------------------------------------------------------------------------------
 // 3. Logout
 //--------------------------------------------------------------------------------------
+//  1) Get post data
+//  2) Update database
+//  3) Return
+//--------------------------------------------------------------------------------------
 
 if (isset($_GET['action']) && $_GET['action'] == 'logout') {
 
-  // Get Post Data
+  // Get post data
   $data = json_decode(file_get_contents("php://input"));
   $token = $data -> token;
   $token = substr($token, 1, strlen($token) - 2); // Why $token has double quote outside?
 
-  // Update Database
+  // Update database
   $info = array('Token' => 'LOGGED OUT');
   $DB -> update($CONFIG['TBL_USER'], $info, "`Token`='" . $token . "'");
 
@@ -101,24 +117,28 @@ if (isset($_GET['action']) && $_GET['action'] == 'logout') {
 //--------------------------------------------------------------------------------------
 // 4. Check Token
 //--------------------------------------------------------------------------------------
+//  1) Get post data
+//  2) Select from database
+//  3) Return
+//--------------------------------------------------------------------------------------
 
 if (isset($_GET['action']) && $_GET['action'] == 'checkToken') {
 
-  // Get Post Data
+  // Get post data
   $data = json_decode(file_get_contents("php://input"));
   $token = $data -> token;
   $token = substr($token, 1, strlen($token) - 2); // Why $token has double quote outside?
 
-  // Select from Database
+  // Select from database
   $sql = "SELECT * FROM `{$CONFIG['TBL_USER']}` WHERE `Token`='$token'";
   $check = $DB -> get_all($sql);
 
+  // Return
   if (count($check) == 1) {
     echo 'authorized';
   } else {
     echo 'unauthorized';
   }
-
 
   exit();
 }
